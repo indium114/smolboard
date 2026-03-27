3492fc24eb13bad354a4225c7cfb5cf081dca88d
indium114 <156162907+indium114@users.noreply.github.com>
2026-03-27 15:04:36 +0200

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "node devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "node-devshell";
+
+          packages = with pkgs; [
+            nodejs
+          ];
+        };
+
+      }
+    );
+}
